d34aac5d2f457d5a7f288a3494b453d52a319af0
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-13 16:42:39 +0200

    installing bootstrap

--- functions.php
+++ functions.php
@@ -5,4 +5,15 @@ function enqueue_custom_styles() {
 
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
 
+// Enqueue Bootstrap CSS and JavaScript
+function enqueue_bootstrap() {
+    // Enqueue Bootstrap CSS
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+    
+    // Enqueue Bootstrap JavaScript (you may also need to enqueue jQuery)
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.min.js', array('jquery'), '5.3.1', true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_bootstrap');
+
+
 ?>
