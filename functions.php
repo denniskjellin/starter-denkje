c32db98deff174b175fea38ddb17fdca2cc9b4c1
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-13 15:46:13 +0200

    edit gitignore

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function enqueue_custom_styles() {
-    wp_enqueue_style('custom-theme-css', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('custom-theme-css', get_template_directory_uri() . '/css/style.css', array(), '1.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
