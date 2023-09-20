081b539307657b68c9f0fdc56c9972540f39675b
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-20 15:47:49 +0200

    changes in script.php

--- inc/scripts.php
+++ inc/scripts.php
@@ -6,20 +6,12 @@
  * @author Dennis Kjellin
  * @package StarterTheme
  */
+
+/* Loading scripts*/
 function enqueue_custom_styles()
 {
     wp_enqueue_style('custom-theme-css', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_script('starter-denkje', get_template_directory_uri() . '/js/main.js');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
-
-// Enqueue Bootstrap CSS and JavaScript (root file js/main.js and style.css), compiled from node_modules with Gulp.
-function enqueue_bootstrap()
-{
-    // Enqueue Bootstrap CSS
-    wp_enqueue_style('starter-denkje', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-
-    // Enqueue Bootstrap JavaScript
-    wp_enqueue_script('start-denkje', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0', true);
-}
-add_action('wp_enqueue_scripts', 'enqueue_bootstrap');
--- index.php
+++ index.php
@@ -6,12 +6,15 @@
  * @author Dennis Kjellin
  */
 get_header();
+
+
+
 ?>
 <div class="container">
     <div id="primary" class="content-area">
         <main id="main" class="site-main row" role="main">
             <div class="col-12">
-                <h1>Powered by index<?php the_title(); ?></h1>
+                <h1>Powered by index <?php the_title(); ?></h1>
                 <?php the_content(); ?>
             </div>
         </main>
