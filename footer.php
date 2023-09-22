6afb28b1cb7a196111ef2eb7d63e28aa5282be9f
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-22 09:05:42 +0200

    foorer dynamic with base styling

--- footer.php
+++ footer.php
@@ -3,9 +3,8 @@
 <footer class="site-footer" role="contentinfo">
     <div class="container p-4">
         <div class="row">
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <!-- <h5 class="text-uppercase">Footer Content</h5> -->
-                <h2 class="text-uppercase h5">
+            <div class="col-lg-6 col-md-12">
+                <h2>
                     <?php echo the_field('heading', 'options') ?>
                 </h2>
                 <p>
@@ -13,9 +12,9 @@
                 </p>
             </div>
             <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h3 class="text-uppercase h5">
+                <h3>
                     <?php echo the_field('link_title', 'options'); ?></h3>
-                <ul class="list-unstyled">
+                <ul>
                     <?php
                     if (have_rows('link_item', 'options')) :
                         while (have_rows('link_item', 'options')) : the_row();
@@ -23,32 +22,30 @@
                             $list_item_title = get_sub_field('link_item_title', 'options');
                     ?>
                     <li>
-                        <a class="text-black-50 text-decoration-none"
-                            href="<?php echo $list_item_url; ?>"><?php echo $list_item_title; ?></a>
+                        <a href="<?php echo $list_item_url; ?>"><?php echo $list_item_title; ?></a>
                     </li>
                     <?php
                         endwhile;
                     endif;
                     ?>
-
-                    <!-- <li>
-                        <a href="#!" class="text-black-50 text-decoration-none">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-black-50 text-decoration-none">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-black-50 text-decoration-none">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-black-50 text-decoration-none">Link 1</a>
-                    </li> -->
                 </ul>
             </div>
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5>Contact</h5>
-                <ul class="list-unstyled mb-0">
-
+            <div class="col-lg-3 col-md-6">
+                <h3><?php echo the_field('link_title_secondary', 'options'); ?></h3>
+                <ul>
+                    <?php
+                    if (have_rows('link_item_secondary', 'options')) :
+                        while (have_rows('link_item_secondary', 'options')) : the_row();
+                            $list_item_url_secondary = get_sub_field('link_item_secondary_url', 'options');
+                            $list_item_title_secondary = get_sub_field('link_item_secondary_title', 'options');
+                    ?>
+                    <li>
+                        <a href="<?php echo $list_item_url_secondary; ?>"><?php echo $list_item_title_secondary; ?></a>
+                    </li>
+                    <?php
+                        endwhile;
+                    endif;
+                    ?>
                 </ul>
             </div>
         </div>
