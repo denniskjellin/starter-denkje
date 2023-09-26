c573a0285e5d309cf32619f2f3c56fdf8e7da115
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-26 14:55:39 +0200

    changes in single

--- single.php
+++ single.php
@@ -2,7 +2,6 @@
 
 /**
  * The template for displaying a single page
- *
  * @author Dennis Kjellin
  * @package Starter Denkje
  */
@@ -21,13 +20,20 @@ pageBanner();
         <main id="main" class="site-main row" role="main">
             <div class="col-md-12">
                 <?php 
-                /* Conditional check, if there is no page banner */
-                    if (!get_field('page_banner_background_image')) 
+                /* Conditional check
+                If there is no page banner and no page banner title, then display the page title as an h1
+                else display the page title as an h2
+                */
+                if (!get_field('page_banner_background_image') || (!get_field('page_banner_title'))) 
                     {
                         ?>
-                <h1><?php the_title(); ?></h1>
+                <h1 class="h1"><?php the_title(); ?></h1>
                 <?php
-                    }
+                    } else {
+                        ?>
+                <h2 class="h1"><?php the_title(); ?></h2>
+                <?php 
+                }        
                         ?>
                 <?php the_content(); ?>
             </div>
