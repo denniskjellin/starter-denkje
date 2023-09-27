255b6e776a59b54ab0d5227819e02a7543141098
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-27 08:28:15 +0200

    working on hero-image

--- page-home.php
+++ page-home.php
@@ -12,11 +12,14 @@ get_header(); ?>
 <!-- hero image -->
 <?php
     $hero_image = get_field('hero_image', 'option');
+    $hero_headline = get_field('hero_headline', 'option');
+    $hero_paragraph = get_field('hero_paragraph', 'option');
     ?>
 <div class="hero-container p-0">
     <img src="<?php echo $hero_image['url']; ?>" alt="<?php echo $hero_image['alt']; ?>" />
     <div class="hero-text">
-        <h1>Hello WOrld</h1>
+        <h1><?php echo $hero_headline; ?></h1>
+        <p><?php echo $hero_paragraph; ?></p>
         <button class="btn btn-primary">Click me</button>
     </div>
 </div>
