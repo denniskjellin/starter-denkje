dada587d66c6b28ba00eb487dbbb72929cfbd5eb
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-27 11:24:16 +0200

    changes in page

--- page.php
+++ page.php
@@ -1,19 +1,40 @@
 <?php
 
 /**
- * The template for displaying all pages
- *
+ * The template for displaying a page
  * @author Dennis Kjellin
- * @package StarterTheme
+ * @package Starter Denkje
  */
 
-get_header(); ?>
+get_header(); 
+
+/* Get the pager banner image if there is one */
+if (get_field('page_banner_background_image')) 
+{
+pageBanner();
+}
+?>
 
 <div class="container pt-5">
     <div id="primary" class="content-area">
         <main id="main" class="site-main row" role="main">
             <div class="col-md-12">
-                <h1>Powered by page<?php the_title(); ?></h1>
+                <?php 
+                /* Conditional check
+                If there is no page banner and no page banner title, then display the page title as an h1
+                else display the page title as an h2
+                */
+                if (!get_field('page_banner_background_image') || (!get_field('page_banner_title'))) 
+                    {
+                        ?>
+                <h1 class="h1"><?php the_title(); ?></h1>
+                <?php
+                    } else {
+                        ?>
+                <h2 class="h1"><?php the_title(); ?></h2>
+                <?php 
+                }        
+                        ?>
                 <?php the_content(); ?>
             </div>
         </main>
