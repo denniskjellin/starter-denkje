42f1d7b8441fd3e3cdf452a9195472889f6da62d
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-27 09:09:48 +0200

    hero image components completed

--- page-home.php
+++ page-home.php
@@ -14,13 +14,15 @@ get_header(); ?>
     $hero_image = get_field('hero_image', 'option');
     $hero_headline = get_field('hero_headline', 'option');
     $hero_paragraph = get_field('hero_paragraph', 'option');
+    $hero_button = get_field('hero_button', 'option');
     ?>
 <div class="hero-container p-0">
     <img src="<?php echo $hero_image['url']; ?>" alt="<?php echo $hero_image['alt']; ?>" />
     <div class="hero-text">
         <h1><?php echo $hero_headline; ?></h1>
         <p><?php echo $hero_paragraph; ?></p>
-        <button class="btn btn-primary">Click me</button>
+        <a href="<?php echo $hero_button['hero_button_url']['url'] ;?>" class="btn btn-primary" role="button"
+            aria-label="<?php echo $hero_button['hero_button_text'] ;?>"><?php echo $hero_button['hero_button_text'] ;?></a>
     </div>
 </div>
 
@@ -33,7 +35,6 @@ get_header(); ?>
                 <h1>Powered by page<?php the_title(); ?></h1>
                 <?php the_content(); ?>
             </div>
-
         </main>
     </div>
 </div>
