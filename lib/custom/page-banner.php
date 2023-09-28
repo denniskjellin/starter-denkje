5cf9c87c61f3932c223df6fb0cb5cb0d75d5befe
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-28 13:15:26 +0200

    page-banner

--- lib/custom/page-banner.php
+++ lib/custom/page-banner.php
@@ -9,16 +9,19 @@ function pageBanner() {
     if (!$pageBannerImage) {
         // Provide a hardcoded fallback image path
         $pageBannerImage = get_theme_file_uri('/images/alpcounty.jpeg');
+    } else if (array($pageBannerImage) && !empty($pageBannerImage['sizes']['pageBanner'])) {
+        // Get the image URL from the ACF field
+        $pageBannerImage = $pageBannerImage['sizes']['pageBanner'];
     }
 
     ?>
 <!-- page banner -->
 <div class="page-banner">
-    <div class="page-banner-bg-image" style="background-image: url(<?php echo $pageBannerImage ?>);">
+    <div class="page-banner-bg-image" style="background-image: url(<?php echo esc_url($pageBannerImage); ?>);">
     </div>
     <div class="page-banner-intro">
         <div class="container">
-            <h1><?php the_field('page_banner_title') ?></h1>
+            <h1 class="h1"><?php the_title(); ?></h1>
             <p><?php the_field('page_banner_subtitle') ?></p>
         </div>
     </div>
