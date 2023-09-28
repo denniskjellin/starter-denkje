ebce6105c20ab89906cfbc4d36946378b854b160
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-09-28 07:39:45 +0200

    added news page

---
+++ templates/page-news.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* Template Name: News Page
+ *
+ * @author Dennis Kjellin
+ */
+get_header();
+
+
+
+?>
+<div class="container pt-5">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main row" role="main">
+            <div class="col-12">
+                <h1>Powered by page-news<?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </div>
+        </main>
+    </div>
+</div>
+
+<?php get_footer(); ?>
