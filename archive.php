6b3ea3673e2299e3889c106e5b88aa78d06ac305
Dennis Kjellin <dennis.kjellin@knowit.se>
2023-10-04 11:02:36 +0200

    css

--- archive.php
+++ archive.php
@@ -1,1 +1,79 @@
-<h1>Archive</h1>
+<?php
+/**
+ * The template for displaying a page
+ * @package Starter Denkje
+ */
+
+get_header();
+
+/* Get the pager banner image if there is one */
+pageBanner();
+?>
+
+<div class="container">
+    <?php
+    // Get the previous page URL
+    $previous_page_url = wp_get_referer();
+
+    if ($previous_page_url) {
+        // Get the title of the previous page
+        $previous_page_title = get_the_title(url_to_postid($previous_page_url));
+
+        if ($previous_page_title) { ?>
+    <div class="metabox metabox-position-up metabox-with-home-link">
+        <p>
+            <a class="metabox-blog-home-link" href="<?php echo esc_url($previous_page_url); ?>">
+                Return to <?php echo esc_html($previous_page_title); ?>
+                <span class="bi bi-arrow-left" aria-hidden="true"></span> <!-- Add your Bootstrap icon here -->
+            </a>
+            <span class="metabox-main"><?php the_title(); ?></span>
+        </p>
+    </div>
+    <?php }
+    }
+    ?>
+</div>
+
+<div class="container pt-4">
+    <main id="main" class="site-main" role="main">
+        <section>
+            <div class="row">
+                <article class="col-md-8">
+                    <?php
+                        // Define a custom query to retrieve posts
+                        $custom_query = new WP_Query(array(
+                            'post_type' => 'post', // You can change this to another post type if needed
+                            'posts_per_page' => -1 // Display all posts
+                        ));
+
+                        if ($custom_query->have_posts()) :
+                            while ($custom_query->have_posts()) : $custom_query->the_post();
+                        ?>
+
+                    <article class="post-content">
+                        <h2 class="post-title"><?php the_title(); ?></h2>
+                        <?php 
+                                if (has_post_thumbnail()) {
+                                    the_post_thumbnail('medium');
+                                }
+                                 the_excerpt();
+                            ?>
+                        <a role="button" href="<?php the_permalink(); ?>" class="btn btn-readmore">Read more</a>
+                    </article>
+                    <?php
+                            endwhile;
+                            wp_reset_postdata();
+                        else :
+                            echo 'No posts found';
+                        endif;
+                        ?>
+                </article>
+                <div class="col-md-4">
+                    <?php get_sidebar('news'); ?>
+                </div>
+            </div><!-- .row -->
+        </section><!-- .container -->
+    </main>
+</div>
+
+<?php get_footer(); ?>
